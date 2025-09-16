409b654b6719df6bfcefcd500f0956781ed62963
André Antônio <andreantoniobr@hotmail.com>
2025-09-16 13:25:14 -0300

    fix: small changes in jobrequests

--- backend/apps/jobrequests/api/v1/serializers.py
+++ backend/apps/jobrequests/api/v1/serializers.py
@@ -14,17 +14,30 @@ class JobRequestSerializer(serializers.ModelSerializer):
             'professional',
         ]
         read_only_fields = [
-            "scheduled_date"
+            "scheduled_date",
+            'status',
             "client", 
             "professional",            
         ]    
 
 class JobRequestUpdateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = JobRequest        
+        model = JobRequest 
         fields = [
+            'id',
+            'scheduled_date',
             'message',
             'status',
             'job',
+            'client',
+            'professional',
             'is_deleted',
-        ]
+        ]     
+        read_only_fields = [
+            'id',
+            'scheduled_date',
+            'status',
+            'job',
+            'client', 
+            'professional',            
+        ]  
--- backend/apps/jobrequests/api/v1/viewsets.py
+++ backend/apps/jobrequests/api/v1/viewsets.py
@@ -3,19 +3,22 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from apps.jobrequests.models import JobRequest
 from apps.jobs.models import Job
-from .serializers import JobRequestSerializer
+from .serializers import JobRequestSerializer, JobRequestUpdateSerializer
 from drf_spectacular.utils import extend_schema
+from apps.core.mixins.update_serializer_mixin import UpdateSerializerMixin
 
 @extend_schema(tags=['Job Requests'])
-class JobRequestViewSet(viewsets.ModelViewSet):
-    queryset = JobRequest.objects.all()
+class JobRequestViewSet(UpdateSerializerMixin, viewsets.ModelViewSet):
     serializer_class = JobRequestSerializer
+    update_serializer_class = JobRequestUpdateSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        # O usuário vê apenas suas solicitações ou as que recebeu 
-        user = self.request.user
-        return JobRequest.objects.for_user(user)
+    def get_queryset(self):        
+        if self.action in ['list', 'retrieve']:
+            # O usuário vê apenas suas solicitações ou as que recebeu 
+            user = self.request.user
+            return JobRequest.objects.for_user(user)
+        return JobRequest.objects.all() 
 
     def perform_create(self, serializer):
         # Preenche automaticamente o client e o professional
